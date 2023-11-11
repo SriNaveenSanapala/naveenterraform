eb04052541050f2d65c1f14bc4c00853df0991bc
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-11 13:30:02 +0530

    commiitted

--- module/databases/main.tf
+++ module/databases/main.tf
@@ -1,29 +1,17 @@
 variable "private_subnet_ids" {}
 
-resource "aws_db_instance" "example" {
-  engine               = "mysql"
-  instance_class       = "db.t2.micro"
-  allocated_storage    = 10
-  count                = 1
-
-  username             = "your_db_username"   # Replace with your desired username
-  password             = "your_db_password"   # Replace with your desired password
-
-  # Set other necessary configurations for the database instance...
-
-  db_parameter_group_name = aws_db_parameter_group.example.name  # This should not be set here
+resource "aws_kms_key" "example" {
+description = "Example KMS Key"
 }
 
-resource "null_resource" "configure_db" {
-  depends_on = [aws_db_instance.example]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      aws rds modify-db-instance \
-        --db-instance-identifier ${aws_db_instance.example[0].id} \
-        --db-parameter-group-name ${aws_db_parameter_group.example.name} \
-        --master-username your_master_username \
-        --master-user-password your_master_password
-    EOT
-  }
+resource "aws_db_instance" "default" {
+  allocated_storage             = 10
+  db_name                       = "mydb"
+  engine                        = "mysql"
+  engine_version                = "5.7"
+  instance_class                = "db.t3.micro"
+  manage_master_user_password   = true
+  master_user_secret_kms_key_id = aws_kms_key.example.key_id
+  username                      = "foo"
+  parameter_group_name          = "default.mysql5.7"
 }
