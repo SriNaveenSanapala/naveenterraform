e4b0cb15b593ead08f49f55bafcb2c089f7292e8
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-11 12:06:17 +0530

    committed

--- main.tf
+++ main.tf
@@ -3,27 +3,25 @@ provider "aws" {
 }
 
 module "network" {
-  source              = "./module/network"
+  source              = "./modules/network"
   vpc_cidr_block      = "10.0.0.0/16"
   availability_zones  = ["us-east-1a", "us-east-1b"]
 }
 
 module "instances" {
-  source              = "./module/instances"
-  vpc_id              = module.network.vpc_id
-  public_subnet_ids   = module.network.public_subnet_ids
-  private_subnet_ids  = module.network.private_subnet_ids
+  source               = "./module/instances"
+  vpc_id               = module.network.vpc_id
+  public_subnet_ids    = module.network.public_subnet_ids
+  private_subnet_ids   = module.network.private_subnet_ids
 }
 
-
-
 module "databases" {
-  source              = "./module/databases"
-  vpc_id              = module.network.vpc_id
-  private_subnet_ids  = module.network.private_subnet_ids
+  source               = "./module/databases"
+  vpc_id               = module.network.vpc_id
+  private_subnet_ids   = module.network.private_subnet_ids
 }
 
 module "security" {
-  source              = "./module/security"
-  vpc_id              = module.network.vpc_id
+  source               = "./module/security"
+  vpc_id               = module.network.vpc_id
 }
