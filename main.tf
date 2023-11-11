a43c6a43cb439f9260374df96efe90e9e7962126
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-11 12:07:11 +0530

    committed

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "network" {
-  source              = "./modules/network"
+  source              = "./module/network"
   vpc_cidr_block      = "10.0.0.0/16"
   availability_zones  = ["us-east-1a", "us-east-1b"]
 }
