dc55fe4ef3b837a619908e4b034ea2825b03fcaf
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-12 21:50:58 +0530

    committed

--- module/databases/main.tf
+++ module/databases/main.tf
@@ -1,11 +1,7 @@
-# ./module/databases/main.tf
-
 module "security" {
   source = "../security"
   vpc_id = module.network.vpc_id
-
 }
-
 
 resource "aws_kms_key" "example" {
   description = "Example KMS Key"
@@ -23,7 +19,14 @@ resource "aws_db_instance" "default" {
   username                      = "foo"
   parameter_group_name          = "default.mysql5.7"
   
-  vpc_security_group_ids        =  [module.security.security_group_id]
+  vpc_security_group_ids        = [module.security.security_group_id]
 
-  subnet_ids                    = [aws_subnet.public[0].id] # Use the ID of the public subnet
+  subnet_ids                    = [module.network.public_subnet_ids[0]] # Use the ID of the public subnet
+}
+
+# Explicitly declare the dependency between databases and network modules
+module "databases" {
+  source             = "./module/databases"
+  private_subnet_ids = module.network.private_subnet_ids
+  depends_on         = [module.network]
 }
