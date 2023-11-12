399fa6c2c9a0cc374aaf56e82818e253096fec85
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-12 21:36:10 +0530

    committed

--- module/databases/main.tf
+++ module/databases/main.tf
@@ -1,10 +1,14 @@
 # ./module/databases/main.tf
 
-resource "aws_kms_key" "example" {
-  description = "Example KMS Key"
+module "security" {
+  source = "../security"
+  vpc_id = var.vpc_id
 }
 
 
+resource "aws_kms_key" "example" {
+  description = "Example KMS Key"
+}
 
 resource "aws_db_instance" "default" {
   allocated_storage             = 10
@@ -17,5 +21,8 @@ resource "aws_db_instance" "default" {
   master_user_secret_kms_key_id = aws_kms_key.example.key_id
   username                      = "foo"
   parameter_group_name          = "default.mysql5.7"
-  #vpc_security_group_ids        = [module.security.aws_security_group.example.id]
+  
+  vpc_security_group_ids        =  [module.security.security_group_id]
+
+  subnet_ids                    = [aws_subnet.public[0].id] # Use the ID of the public subnet
 }
