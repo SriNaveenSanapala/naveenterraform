5bbc77a1aad51e751c2a55514ef45d1a66e86f35
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-12 16:17:55 +0530

    first commit

--- module/instances/main.tf
+++ module/instances/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 
   subnet_id = element(var.public_subnet_ids, count.index)
+  security_group_names = [aws_security_group.example.name]
 
   tags = {
     Name = "ExampleInstance-${count.index + 1}"
