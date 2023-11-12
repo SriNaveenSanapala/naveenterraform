644bd03a2f412f11b7dceede2763c3423f6aaa24
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-12 23:55:40 +0530

    first commit

--- module/security/outputs.tf
+++ module/security/outputs.tf
@@ -1,7 +1,3 @@
-
 output "security_group_id" {
-  value = length(aws_security_group.examples) > 0 ? aws_security_group.examples[0].id : null
-}
-output "security_group_id" {
-  value = aws_security_group.examples.id
+  value = var.create_security_group ? aws_security_group.examples[0].id : null
 }
