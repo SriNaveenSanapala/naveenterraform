649330c3e5c391447c045825a029ae2ad10a357b
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-12 16:39:31 +0530

    first commit

--- module/instances/main.tf
+++ module/instances/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 
   subnet_id = element(var.public_subnet_ids, count.index)
-  security_group_names = [aws_security_group.example.name]
+  vpc_security_group_ids = [aws_security_group.example.id] 
 
   tags = {
     Name = "ExampleInstance-${count.index + 1}"
--- module/security/main.tf
+++ module/security/main.tf
@@ -10,6 +10,17 @@ resource "aws_security_group" "example" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  # Create a separate security group rule for RDS
+  resource "aws_security_group_rule" "rds_ingress" {
+    security_group_id = aws_security_group.example.id
+    type              = "ingress"
+    from_port         = 3306
+    to_port           = 3306
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+}
+
 
  
-}
+
