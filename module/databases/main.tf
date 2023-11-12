343b181748c8d6cd5ad8e2c08822d527276fbc3f
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-12 23:38:20 +0530

    committed

--- module/databases/main.tf
+++ module/databases/main.tf
@@ -16,10 +16,11 @@ resource "aws_db_subnet_group" "example" {
   name        = "my-db-subnet-group"
   description = "My DB Subnet Group"
   subnet_ids  = [
-    module.network.aws_subnet.private[0].id,  # Make sure this subnet is in one AZ
+    module.network.subnet_ids.private[0],  # Make sure this subnet is in one AZ
     # Add more subnets if needed
   ]
 }
+
 
 resource "aws_db_instance" "default" {
   allocated_storage             = 10
