b2d5a39e438a662606cb4badb2c3d8715c03b217
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-13 04:38:30 +0530

    committred

--- module/security/main.tf
+++ module/security/main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "ec2_sg" {
   resource "aws_security_group" "rds_sg" {
   name        = "db_sg"
   description = "Security group for database instances"
-  vpc_id      = "var.vpc_id"
+  vpc_id      = var.vpc_id
 
   // Define your security group rules here
   ingress {
