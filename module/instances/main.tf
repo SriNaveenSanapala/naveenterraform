e39df820619682b88dc2bbe886fefa7ac2d90dbe
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-13 00:25:43 +0530

    Revert "first commit"
    This reverts commit 93c9abe29e8502edf6fed7a96f0663b9c63401ab.

--- module/instances/main.tf
+++ module/instances/main.tf
@@ -1,16 +1,15 @@
-module "security" {
-  source = "../security"
-  vpc_id = var.vpc_id
-}
-
 resource "aws_instance" "example" {
-  count                = 2
-  ami                  = ""
-  instance_type        = "t2.micro"
-  subnet_id            = element(var.public_subnet_ids, count.index)
-  vpc_security_group_ids = [module.security.security_group_id]
+  count         = 2
+  ami           = "ami-05c13eab67c5d8861"
+  instance_type = "t2.micro"
+
+  subnet_id = element(var.public_subnet_ids, count.index)
+  vpc_security_group_ids = [aws_security_group.example.id] 
 
   tags = {
     Name = "ExampleInstance-${count.index + 1}"
+    # Add other tags as needed
   }
+
+  
 }
