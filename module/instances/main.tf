c3610f728ffe2f7751202d3ce052a3072ce3f993
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-13 00:51:28 +0530

    committed

--- module/instances/main.tf
+++ module/instances/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "example" {
   ami                  = "ami-05c13eab67c5d8861"
   instance_type        = "t2.micro"
   subnet_id            = element(var.public_subnet_ids, count.index)
-  vpc_security_group_ids = [module.security.security_group_id] # Use vpc_security_group_ids instead
+   # Use vpc_security_group_ids instead
 
   tags = {
     Name = "ExampleInstance-${count.index + 1}"
