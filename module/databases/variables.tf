96c8a2ab39c93367a5459803791e0b799538ccde
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-12 22:47:38 +0530

    committed

--- module/databases/variables.tf
+++ module/databases/variables.tf
@@ -1,1 +1,7 @@
 variable "private_subnet_ids" {}
+variable "public_subnet_ids" {
+  type    = list(string)
+  default = []  # Add a default value or replace it with your actual default values
+}
+
+# ... rest of the configuration
