4804d4810b46bce18b800c6cc1c7b2ae7a842eed
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-12 18:02:34 +0530

    first commit

--- module/instances/main.tf
+++ module/instances/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "example" {
   ami                  = "ami-05c13eab67c5d8861"
   instance_type        = "t2.micro"
   subnet_id            = element(var.public_subnet_ids, count.index)
-  vpc_security_group_ids = [var.security_group_id] # Change to use var
+  #vpc_security_group_ids = [var.security_group_id] # Change to use var
   tags = {
     Name = "ExampleInstance-${count.index + 1}"
   }
