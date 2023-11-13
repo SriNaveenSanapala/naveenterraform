c6cd5e94982f9e63c53376ea4ba3953d959f1540
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-13 06:48:12 +0530

    committed

--- module/databases/main.tf
+++ module/databases/main.tf
@@ -1,8 +1,8 @@
 // databases/main.tf
 
 resource "aws_db_subnet_group" "example" {
-  name        = "my-new-db-subnet-group-4"
-  description = "my-new-db-subnet-group-4"
+  name        = "my-new-db-subnet-group-5"
+  description = "my-new-db-subnet-group-5"
   subnet_ids  = var.private_subnet_ids
 
   tags = {
--- module/security/main.tf
+++ module/security/main.tf
@@ -10,6 +10,12 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust this to the appropriate IP range or ranges
+  }
   ingress {
     from_port   = 3306
     to_port     = 3306
@@ -33,6 +39,12 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   }
